e3e5eed3dd2e8d13deed1afe4a54154f50be4bc1
kartik <kr816473@gmail.com>
2024-05-16 15:23:49 +0530

    update

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "भारत संयुक्त राष्ट्र के कितने शांति मिशन में हिस्सा ले चुका है ?",
+    answer: "49",
+    options: [
+      "58",
+      "60",
+      "49",
+      "57"
+    ]
+  },
+  {
+    numb: 2,
+    question: "कारगिल युद्ध कब लड़ा गया था ?",
+    answer: "1999",
+    options: [
+      "1998",
+      "1999",
+      "2001",
+      "1992"
+    ]
+  },
+  {
+    numb: 3,
+    question: "भारतीय सेना की ट्रेनिंग कमान का मुख्यालय कहां है?",
+    answer: "शिमला",
+    options: [
+      "शिमला",
+      "मथुरा",
+      "आगरा",
+      "हैदराबाद"
+    ]
+  },
+  {
+    numb: 4,
+    question: "भारतीय सेना की स्थापना कब हुई थी?",
+    answer: "1895",
+    options: [
+      "1992",
+      "1892",
+      "1997",
+      "1895"
+    ]
+  },
+  {
+    numb: 5,
+    question: "ARMY का फुल फॉर्म क्या होता है?",
+    answer: "Alert Regular Mobility Young",
+    options: [
+      "A Regular Mobility Young",
+      "Alert Regular Mobility Young",
+      "Alerternate Regular Mobility Young",
+      "Akhilesh Rana Mobility Young"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
